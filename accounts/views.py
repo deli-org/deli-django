751df2e1f32d2e3feb9f9cbe97f659bb1a158d81
gil-air-may <gilairmay@gmail.com>
2020-11-04 09:17:15 -0300

    feat: add Login view

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from django.contrib.auth import authenticate
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.authtoken.models import Token
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+
+class Login(APIView):
+    def post(self, request):
+        user = authenticate(
+            username=request.data['username'], password=request.data['password'])
+
+        if user is not None:
+            token = Token.objects.get_or_create(user=user)[0]
+            return Response({'token': token.key})
+
+
+class Protected(APIView):
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        return Response({'org': request.user.org.name})
