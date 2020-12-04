06b836e5a7f24d14839e03e5bd26e652c78cc5ee
gil-air-may <gilairmay@gmail.com>
2020-12-04 08:56:46 -0300

    feat: adapt sale serializer

--- sales/serializers.py
+++ sales/serializers.py
@@ -1,16 +1,37 @@
 from rest_framework import serializers
 from .models import Sale, SaleDetail
+from products.serializers import ProductSerializer
+import ipdb
 
 
-class SaleDetailSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = SaleDetail
-        fields = ('product_id', 'amount', 'unitprice_id', 'discount')
+class SaleDetailSerializer(serializers.Serializer):
+    # READ AND WRITE
+    product_id = serializers.IntegerField()
+    amount = serializers.IntegerField()
+    unitprice_id = serializers.IntegerField()
+    discount = serializers.IntegerField()
+
+    # READ ONLY
+    product = serializers.SerializerMethodField('get_product')
+
+    def get_product(self, saledetail):
+        product = saledetail.product
+        return ProductSerializer(product).data
 
 
-class SaleSerializer(serializers.ModelSerializer):
-    saledetails = SaleDetailSerializer(many=True, read_only=False)
+class SaleSerializer(serializers.Serializer):
+    # READ AND WRITE
+    saledetails = serializers.SerializerMethodField(
+        'get_saledetails', read_only=False)
+    identifier = serializers.CharField()
+    paid = serializers.BooleanField()
+    payment_type = serializers.CharField()
 
-    class Meta:
-        model = Sale
-        fields = ('paid', 'identifier', 'payment_type', 'saledetails')
+    # READ ONLY
+    id = serializers.IntegerField(read_only=True)
+    updated_at = serializers.DateTimeField(read_only=True)
+    created_at = serializers.DateTimeField(read_only=True)
+
+    def get_saledetails(self, sale):
+        saledetails = sale.saledetails
+        return SaleDetailSerializer(saledetails, many=True).data
